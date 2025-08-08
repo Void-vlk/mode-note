4ab8657c24d6deae007fc49bd7598a191b606725
Void-vlk <druurgzorvox@gmail.com>
2025-08-08 17:28:36 +0100

    Reapply "Fix positioning of LR & add transition"
    This reverts commit 92f01534ec9903d4ad077df027efb7ceef288a59.

--- src/resources/note-pitches.ts
+++ src/resources/note-pitches.ts
@@ -16,17 +16,54 @@ export const NOTE_PITCHES: Record<NotePitch, NoteVariant> = {
   11: { natural: "B" },
 } as const;
 
-export const INTERVALS: Record<NotePitch, NoteVariant> = {
-  0: { natural: "I" },
-  1: { sharp: "♯I", flat: "♭II" },
-  2: { natural: "II" },
-  3: { sharp: "♯II", flat: "♭III" },
-  4: { natural: "III" },
-  5: { natural: "IV" },
-  6: { sharp: "♯IV", flat: "♭V}" },
-  7: { natural: "V" },
-  8: { sharp: "♯V", flat: "♭VI" },
-  9: { natural: "VI" },
-  10: { sharp: "♯VI", flat: "♭VII" },
-  11: { natural: "VII" },
+export const INTERVALS: Record<
+  NotePitch,
+  { major: NoteVariant; minor: NoteVariant }
+> = {
+  0: { major: { natural: "I" }, minor: { natural: "i" } },
+  1: {
+    major: { sharp: "♯I", flat: "♭II" },
+    minor: { sharp: "♯i", flat: "♭ii" },
+  },
+  2: { major: { natural: "II" }, minor: { natural: "ii" } },
+  3: {
+    major: { sharp: "♯II", flat: "♭III" },
+    minor: { sharp: "♯ii", flat: "♭iii" },
+  },
+  4: { major: { natural: "III" }, minor: { natural: "iii" } },
+  5: { major: { natural: "IV" }, minor: { natural: "iv" } },
+  6: {
+    major: { sharp: "♯IV", flat: "♭V" },
+    minor: { sharp: "♯iv", flat: "♭v" },
+  },
+  7: { major: { natural: "V" }, minor: { natural: "v" } },
+  8: {
+    major: { sharp: "♯V", flat: "♭VI" },
+    minor: { sharp: "♯v", flat: "♭vi" },
+  },
+  9: { major: { natural: "VI" }, minor: { natural: "vi" } },
+  10: {
+    major: { sharp: "♯VI", flat: "♭VII" },
+    minor: { sharp: "♯vi", flat: "♭vii" },
+  },
+  11: { major: { natural: "VII" }, minor: { natural: "vii" } },
+  } as const;
+
+export type Mode =
+  | "Ionian"
+  | "Dorian"
+  | "Phrygian"
+  | "Lydian"
+  | "Mixolydian"
+  | "Aeolian"
+  | "Locrian";
+
+export const MODE_INTERVALS_SIMPLE: Record<Mode, string[]> = {
+  Ionian: ["I", "ii", "iii", "IV", "V", "vi", "vii°"],
+  Dorian: ["i", "ii", "III", "IV", "v", "vi°", "VII"],
+  Phrygian: ["i", "II", "III", "iv", "v°", "VI", "vii"],
+  Lydian: ["I", "II", "iii", "♯iv°", "V", "vi", "vii"],
+  Mixolydian: ["I", "ii", "iii°", "IV", "v", "vi", "♭VII"],
+  Aeolian: ["i", "ii°", "♭III", "iv", "v", "♭VI", "♭VII"],
+  Locrian: ["i°", "♭II", "♭III", "iv", "♭V", "♭VI", "♭vii"],
 } as const;
